77bd657662eb03f20275d0ab1225558dc528735e
ObiwanBox <connorknabe@gmail.com>
2017-11-06 07:55:55 -0500

    Adding loginexample and gitignore

---
+++ loginExample.js
@@ -0,0 +1,6 @@
+module.exports.TWILIO_AUTH_TOKEN = 'asdfasdf2342flkasdjflkajsdf';
+module.exports.TWILIO_ACCOUNT_SID = 'a123412sdfasdf2342flkasdjflkajsdf';
+module.exports.toNumber = '3145555111';
+module.exports.fromNumber = '5735554113';
+module.exports.facebookVerifyToken =
+    'asdfkjasdf02j30fj20efj02je90fj092hf082hefhipdfpij';
